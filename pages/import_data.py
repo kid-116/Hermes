3e4c8b0ffcc2bdf5dc05dac45a0fa1c47b0912e2
Mehul Todi <mehultodi116@gmail.com>
2024-02-25 23:31:46 +0530

    Infer and display schema

--- pages/import_data.py
+++ pages/import_data.py
@@ -19,20 +19,37 @@ def import_page():
         table_names = data_import.get_tables(project['folder'])
 
         status.write('Loading tables...')
-        table_data = data_import.load_tables(project['folder'], table_names)
+        table_data = {}
+        for name in table_names:
+            table_data[name] = data_import.load_table(project['folder'], name)
+
+        status.write('Infering schema...')
+        table_schemas = {}
+        for name in table_names:
+            table_schemas[name] = data_import.get_schema(table_data[name])
 
         for name in table_names:
             st.subheader(name)
-            data_tab, schema_tab = st.tabs(['Data', 'Schema'])
+            data_tab, schema_tab = st.columns([3, 1])
             with data_tab:
-                st.dataframe(table_data[name].head(
-                    constants.DATAFRAME_DISP_SIZE))
+                st.markdown('##### Data')
+                st.dataframe(
+                    table_data[name].head(constants.DATAFRAME_DISP_SIZE),)
             with schema_tab:
-                st.write('Coming soon...')
+                st.markdown('##### Schema')
+                schema = table_schemas[name]
+                st.json(schema, expanded=False)
 
         status.update(label='Import complete!',
                       state='complete',
                       expanded=False)
 
 
-auth.navbar(import_page)
+def import_page_wrapper():
+    if constants.ACTIVATE_PROJECT in st.session_state:
+        import_page()
+    else:
+        st.error('No project selected')
+
+
+auth.navbar(import_page_wrapper)
--- src/data_import.py
+++ src/data_import.py
@@ -1,6 +1,17 @@
+from enum import Enum
 import os
 
 import pandas as pd
+
+import constants
+
+
+class ColumnType(Enum):
+    INTEGER = 1, 'INTEGER'
+    FLOAT = 2, 'FLOAT'
+    STRING = 3, 'STRING'
+    ENUM = 4, 'ENUM'
+    UNKNOWN = 5, 'UNKNOWN'
 
 
 def get_tables(_dir):
@@ -8,8 +19,53 @@ def get_tables(_dir):
     return [file.strip('.csv') for file in files if file.endswith('.csv')]
 
 
-def load_tables(_dir, files):
-    data = {}
-    for file in files:
-        data[file] = pd.read_csv(os.path.join(_dir, file + '.csv'))
-    return data
+def load_table(_dir, name):
+    return pd.read_csv(os.path.join(_dir, name + '.csv'))
+
+
+def get_columns(df):
+    return df.columns
+
+
+def infer_column_type(df, column):
+    match df[column].dtype:
+        case 'int64':
+            return ColumnType.INTEGER
+        case 'float64':
+            return ColumnType.FLOAT
+        case 'object':
+            n_unique = len(df[column].unique())
+            if n_unique / len(
+                    df
+            ) < constants.ENUM_FRACTION_THRESHOLD and n_unique < constants.ENUM_N_THRESHOLD:
+                return ColumnType.ENUM
+            return ColumnType.STRING
+        case _:
+            return ColumnType.UNKNOWN
+
+
+def get_schema(df):
+    columns = get_columns(df)
+    schema = {}
+    for column in columns:
+        schema[column] = {}
+
+        _type = infer_column_type(df, column)
+        schema[column]['type'] = _type
+
+        if _type == ColumnType.ENUM:
+            schema[column]['values'] = list(df[column].unique())
+
+        schema[column]['nullable'] = bool(df[column].isnull().sum() > 0)
+        if _type in [ColumnType.ENUM, ColumnType.STRING]:
+            schema[column]['has_blanks'] = len(df[df[column] == ''].index) > 0
+
+        if len(df[column].unique()) == len(df):
+            schema[column]['unique'] = True
+        if not schema[column]['nullable'] and not 'has_blanks' in schema[
+                column] and 'unique' in schema[column]:
+            schema[column]['candidate_key'] = True
+
+        schema[column]['type'] = str(_type.name)
+
+    return schema
