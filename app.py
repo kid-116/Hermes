cc2f21a5524e756a14063e01a5511180dad879ab
Mehul Todi <mehultodi116@gmail.com>
2024-02-25 14:05:31 +0530

    Fix quotes

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 logger = st.logger.get_logger(__name__)
 
-st.set_page_config(page_title="Hermes", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title='Hermes', page_icon=':bar_chart:', layout='wide')
