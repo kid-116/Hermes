b65679f59ce16dc9c0b29944d49a35977b854a17
Mehul Todi <mehultodi116@gmail.com>
2024-03-27 21:33:18 +0530

    Setup for Streamlit cloud

--- models/utils.py
+++ models/utils.py
@@ -1,8 +1,10 @@
+import json
 import os
 
 import firebase_admin  # type: ignore[import-untyped]
 from firebase_admin import firestore
 from firebase_admin import credentials
+import streamlit as st
 
 import constants
 
@@ -13,9 +15,12 @@ os.environ[
 class Firestore:  # pylint: disable=too-few-public-methods
 
     def __init__(self, col_name: str):
+
         if not firebase_admin._apps:  # pylint: disable=protected-access
             cred = credentials.Certificate(
-                constants.GOOGLE_APPLICATION_CREDENTIALS)
+                constants.GOOGLE_APPLICATION_CREDENTIALS if os.path.
+                isfile('credentials.json') else json.loads(st.secrets.
+                                                           FIRESTORE_CERT))
             firebase_admin.initialize_app(cred)
 
         db = firestore.Client()  # pylint: disable=no-member
